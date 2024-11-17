5f2584675e1709ed1789a4a58009d189459b7a91
Basil Ugo <basilugo2@gmail.com>
2024-11-17 15:11:27 +0100

    init page

---
+++ safeguard-next/src/app/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LandingPage from "./pages/landing/page";
+
+const page = () => {
+	return (
+		<div>
+			<LandingPage />
+		</div>
+	);
+};
+
+export default page;
