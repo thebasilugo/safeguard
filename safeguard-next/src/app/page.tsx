e25834cb2926fa9cfb6d5c4cb33a2b4c52c011cd
Basil Ugo <basilugo2@gmail.com>
2024-11-17 15:14:58 +0100

    init landing page

--- safeguard-next/src/app/page.tsx
+++ safeguard-next/src/app/page.tsx
@@ -1,10 +1,24 @@
 import React from "react";
 import LandingPage from "./pages/landing/page";
+import LoginPage from "./auth/login/page";
+import SignupPage from "./auth/signup/page";
+import Dashboard from "./pages/dashboard/page";
+import CybersecurityTips from "@/components/CyberTips";
+import PasswordStrengthChecker from "@/components/PasswordStrengthChecker";
+import PasswordGenerator from "@/components/PasswordGenerator";
+import BruteForceVisualTool from "@/components/BruteForceVisualTool";
 
 const page = () => {
 	return (
 		<div>
 			<LandingPage />
+			{/* <LoginPage /> */}
+			{/* <SignupPage /> */}
+			{/* <Dashboard /> */}
+			{/* <CybersecurityTips /> */}
+			{/* <PasswordStrengthChecker /> */}
+			{/* <PasswordGenerator /> */}
+			{/* <BruteForceVisualTool /> */}
 		</div>
 	);
 };
