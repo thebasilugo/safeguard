6bfc2be89d5b49e952b4c2d733c983cf71ac2998
Basil Ugo <basilugo2@gmail.com>
2024-11-19 21:18:49 +0100

    minor update

--- safeguard-next/src/app/layout.tsx
+++ safeguard-next/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 // import localFont from "next/font/local";
 import "./globals.css";
-import Header from "@/components/Header";
+// import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
 // const geistSans = localFont({
