ab4d0557fc923f0d25b0e85fe479f9f90d043e71
Basil Ugo <basilugo2@gmail.com>
2025-08-26 12:59:45 +0100

    add client for page

--- safeguard-next/src/app/page.tsx
+++ safeguard-next/src/app/page.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import LandingPage from "./pages/landing/page";
 import LoginPage from "./auth/login/page";
 import SignupPage from "./auth/signup/page";
