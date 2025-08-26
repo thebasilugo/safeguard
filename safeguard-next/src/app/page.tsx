ad9c89ad8eca613a761ddb977b17a781e32107c4
Basil Ugo <basilugo2@gmail.com>
2025-08-26 13:00:07 +0100

    add enhanced password gen

--- safeguard-next/src/app/page.tsx
+++ safeguard-next/src/app/page.tsx
@@ -6,7 +6,7 @@ import SignupPage from "./auth/signup/page";
 import Dashboard from "./pages/dashboard/page";
 import CybersecurityTips from "@/components/CyberTips";
 import PasswordStrengthChecker from "@/components/PasswordStrengthChecker";
-import PasswordGenerator from "@/components/PasswordGenerator";
+import EnhancedPasswordGenerator from "@/components/PasswordGenerator";
 import BruteForceVisualTool from "@/components/BruteForceVisualTool";
 
 const page = () => {
@@ -18,8 +18,20 @@ const page = () => {
 			{/* <Dashboard /> */}
 			{/* <CybersecurityTips /> */}
 			{/* <PasswordStrengthChecker /> */}
-			{/* <PasswordGenerator /> */}
 			{/* <BruteForceVisualTool /> */}
+
+			{/* Conditionally render the password generator modal */}
+			{showPasswordGenerator && (
+				<EnhancedPasswordGenerator onClose={handleClosePasswordGenerator} />
+			)}
+
+			<button
+				onClick={() => setShowPasswordGenerator(true)}
+				className="bg-gray-900 hover:border-gray-900 border text-white px-4 py-2 rounded-md fixed top-8 right-8"
+			>
+				{/* className="bg-gray-900 hover:border-white hover:text-gray-900 hover:bg-white text-white px-4 py-2 rounded-md fixed top-8 right-8" */}
+				Open Password Generator
+			</button>
 		</div>
 	);
 };
